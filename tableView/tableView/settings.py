be055185b67d96fd7f067252428e8dc5d6c318f9
ShamlaHameed123 <shamlahameed95@gmail.com>
2021-01-28 19:33:04 +0530

    added corsheader config in settings, template changed

--- tableView/tableView/settings.py
+++ tableView/tableView/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CORS_ORIGIN_ALLOW_ALL=True
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tableApp',
     'rest_framework_datatables',
+	'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
